b42f7b84f792bc6d6c18729fcc937b923da1e1a7
wayand <lawangjan@hotmail.com>
2015-09-16 10:23:13 +0200

    Update ProductDescriptionController.php

--- app/Http/Controllers/ProductDescriptionController.php
+++ app/Http/Controllers/ProductDescriptionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Description;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -14,9 +14,9 @@ class ProductDescriptionController extends Controller
      *
      * @return Response
      */
-    public function index()
+    public function index($productId)
     {
-        //
+        return Description::ofProduct($productId)->paginate();
     }
 
     /**
