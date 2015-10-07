bb83ee9c39537d35e86e0f6ef9c3afcb85c4fcd8
Adam Wathan <adam.wathan@gmail.com>
2015-10-07 08:58:52 -0400

    Make testProductCreation pass

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,10 +6,10 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
-    use DatabaseTransactions;
-    
+    use DatabaseMigrations;
+
     protected $jsonHeaders = ['Content-Type' => 'application/json', 'Accept' => 'application/json'];
-    
+
     /**
      * A basic functional test example.
      *
@@ -20,36 +20,36 @@ class ExampleTest extends TestCase
         $this->visit('/')
              ->see('Laravel 5');
     }
-    
+
     public function testProductsList()
     {
         $products = factory(Product::class, 3)->create();
         $this->get(route('api.products.index'))
              ->assertResponseOk();
-        
+
         array_map(function($product){
             $this->seeJson($product->jsonSerialize());
         }, $products->all());
     }
-    
+
     public function testProductDescriptionsList()
     {
         $product = factory(Product::class)->create();
         $product->descriptions()->saveMany(factory(Description::class, 3)->make());
-        
+
         $this->get(route('api.products.descriptions.index', ['products' => $product->id]))
              ->assertResponseOk();
-        
+
         array_map(function($description){
             $this->seeJson($description->jsonSerialize());
         }, $product->descriptions->all());
     }
-    
+
     public function testProductCreation()
     {
         $product = factory(App\Product::class)->make(['name' => 'meats']);
-        
-        $this->post(route('api.products.store'), $product->jsonSerialize(), $this->jsonHeaders)
+
+        $this->json('POST', route('api.products.store'), $product->jsonSerialize())
         ->seeInDatabase('products', ['name' => $product->name])
         ->assertResponseOk();
     }
